9fd58c4cbd10d929af65693447c7ca0f3b00cade
William Minshew <wminshew@gmail.com>
2020-02-12 17:44:35 -0800

    added board logic

--- src/App.js
+++ src/App.js
@@ -5,76 +5,45 @@ import { Header } from "./components/Header";
 import { Board } from "./components/Board";
 import { Card } from "./components/Card";
 
-const cards = [
-  {
-    id: 1,
-    content: "Content?",
-    color: "#DDD"
-  },
-  {
-    id: 2,
-    content: "Content?",
-    color: "#DDD"
-  },
-  {
-    id: 3,
-    content: "Content?",
-    color: "#747298"
-  },
-  {
-    id: 4,
-    content: "Content?",
-    color: "#DDD"
-  },
-  {
-    id: 5,
-    content: "Content?",
-    color: "#39F"
-  },
-  {
-    id: 6,
-    content: "Content?",
-    color: "#DDD"
-  },
-  {
-    id: 7,
-    content: "Content?",
-    color: "#DDD"
-  },
-  {
-    id: 8,
-    content: "Content?",
-    color: "#DDD"
-  },
-  {
-    id: 9,
-    content: "Content?",
-    color: "#DDD"
-  },
-  {
-    id: 10,
-    content: "Content?",
-    color: "#DDD"
-  },
-  {
-    id: 11,
-    content: "Content?",
-    color: "#DDD"
-  },
-  {
-    id: 12,
-    content: "Content?",
-    color: "#DDD"
-  }
-];
+import { WORDS } from "./words.js"
+const GAME_SIZE = 25;
+const SOLUTIONS_COUNT = [ 9, 8, 7, 1 ];
+const SOLUTIONS_COLORS = [
+  "#a83236",
+  "#1d18ab",
+  "#baad18",
+  "#000000",
+]
 
-function App() {
+const SeededShuffle = require('seededshuffle');
+const seed = Date.now();
+
+const words = [];
+for(; words.length < GAME_SIZE; ) {
+  var word;
+  do {
+    word = WORDS[Math.floor(Math.random() * WORDS.length)];
+  } while (words.includes(word));
+  words.push(word);
+}
+
+const solutions = SOLUTIONS_COUNT.flatMap( (v, i) =>
+  Array(v).fill(i) );
+
+const first = Math.round(Math.random()); // 0 or 1
+if ( first === 1 ) {
+  solutions[SOLUTIONS_COUNT[0] - 1] = first;
+}
+
+const shuffledSolutions = SeededShuffle.shuffle(solutions, seed, true);
+
+const App = () => {
   return (
     <div className="App">
       <Header />
       <Board>
-        {cards.map(i => {
-          return <Card key={i.id} color={i.color} content={i.content} />;
+        {shuffledSolutions.map( (v, i) => {
+          return <Card key={i} color={SOLUTIONS_COLORS[v]} content={words[i]} />;
         })}
       </Board>
     </div>
